fcc49898bc133e833bbcd4636ef5fd078153ac3a
Francisco Segui <103472115+FranciscoSegui100@users.noreply.github.com>
2026-04-29 14:11:46 -0300

    Add files via upload

---
+++ jSwing/Tp1_formulario.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_formulario;
+
+/**
+ *
+ * @author juanjorge
+ */
+public class Tp1_formulario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
